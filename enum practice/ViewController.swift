92d54e612e9d75a5a98b5c4538e2e4298bb4472c
Jason McCoy <jasonmccoy@Jasons-MacBook-Pro.local>
2016-07-10 23:45:03 -0700

    Added number tags to cases and then assigned them
    Don’t be offended by the label comments!

--- enum practice/ViewController.swift
+++ enum practice/ViewController.swift
@@ -9,6 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayTextLbl: UILabel!
+    
+    enum Cars: Int {
+        case scionFRSRocketBunny = 0
+        case Mk5Gti = 1
+        case blueGlowLambo = 2
+        case redFerrari = 3
+    }
+    
+    var carOfChoice: Cars!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +33,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onCarBtnTapped(_ sender: AnyObject) {
+        
+        if sender.tag == Cars.scionFRSRocketBunny.rawValue {
+            displayTextLbl.text = "Takes off like a rabbit!"
+        } else if sender.tag == Cars.Mk5Gti.rawValue {
+            displayTextLbl.text = "Trash Euro car."
+        } else if sender.tag == Cars.blueGlowLambo.rawValue {
+            displayTextLbl.text = "Mother of god..."
+        } else if sender.tag == Cars.redFerrari.rawValue {
+            displayTextLbl.text = "VROOOM..."
+        }
+    }
 }
 
